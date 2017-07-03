64d6747fd2e34217320b5dfdb228d8620c6736b8
Francesco <francesc.amoroso@he-arc.ch>
2017-07-03 16:52:19 +0200

    Initialisation de router et hello.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,39 +1,119 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <input type="text" v-model="uneAutreVar">
+    <button v-on:click="addToList">Ajouter</button>
+    <label>Filtre
+      <input v-model="query">
+    </label>
+    <h1 v-if="on">
+      {{ message }}
+    </h1>
+  
+    <h1 v-show="on">
+      {{ uneAutreVar }}
+    </h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li v-for="(person, index) in defaultList" v-bind:key="index" v-bind:style="{color: person.color}" v-bind:class="{red: index === 1}">
+        #{{ index + 1 }} {{ person.name }}
+  
+        <img v-bind:src="person | toAvatars">
+        <button v-on:click="remove(person)">Supprimer</button>
+      </li>
     </ul>
-    <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="(person, index) in listFiltre" v-bind:key="index" v-bind:style="{color: person.color}" v-bind:class="{red: index === 1}">
+        #{{ index + 1 }} {{ person.name }}
+  
+        <img v-bind:src="person | toAvatars">
+        <button v-on:click="remove(person)">Supprimer</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+const LOCALSTORAGE_KEY = 'vuedemo-malist';
+
 export default {
     name: 'hello',
     data() {
+        let maList;
+        try {
+            maList = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+            if (!maList) {
+                throw new Error();
+            }
+        } catch (e) {
+            maList = [
+                {
+                    name: 'Bibi',
+                    color: '#FF00FF'
+                },
+                {
+                    name: 'Moulin',
+                    color: '#00FF00'
+                },
+                {
+                    name: 'Babe',
+                    color: '#0000FF'
+                }
+            ];
+        }
         return {
-            msg: 'Welcome to Your Vue.js App'
+            on: true,
+            query: '',
+            uneAutreVar: 'Bibi',
+            message: 'Hello vue.js',
+            maList
         };
+    },
+    filters: {
+        toAvatars(person) {
+            return `https://robohash.org/${person.name}`;
+        }
+    },
+    methods: {
+        addToList() {
+            const person = {
+                name: this.uneAutreVar,
+                color: this.color
+            };
+            this.maList.push(person);
+        },
+        remove(person) {
+            let index = this.maList.indexOf(person);
+            this.maList.splice(index, 1);
+        }
+    },
+    computed: {
+        listFiltre() {
+            return this.maList.filter(person => {
+                return person.name.toLowerCase().indexOf(this.query.toLowerCase()) === 0;
+            });
+        }
+    },
+    watch: {
+        maList: {
+            handler: function (val, oldval) {
+                console.log('qqch a changé');
+                const listJson = JSON.stringify(this.maList);
+                localStorage.setItem(LOCALSTORAGE_KEY, listJson);
+            },
+            deep: true
+        }
     }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+.red {
+  background-color: rosybrown;
+}
+
+h1,
+h2 {
   font-weight: normal;
 }
 
--- src/main.js
+++ src/main.js
@@ -1,63 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-// import App from './App';
-// import router from './router';
+import App from './App';
+import router from './router';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    data() {
-        return {
-            on: true,
-            query: '',
-            uneAutreVar: 'Bibi',
-            message: 'Hello vue.js',
-            maList: [
-                {
-                    name: 'Bibi',
-                    color: '#FF00FF'
-                },
-                {
-                    name: 'Moulin',
-                    color: '#00FF00'
-                },
-                {
-                    name: 'Babe',
-                    color: '#0000FF'
-                }
-            ]
-        };
-    },
-    filters: {
-        toAvatars(person) {
-            return `https://robohash.org/${person.name}`;
-        }
-
-    },
-    methods: {
-        addToList() {
-            const person = {
-                name: this.uneAutreVar,
-                color: this.color
-            };
-            this.maList.push(person);
-        },
-        remove(person) {
-            let index = this.maList.indexOf(person);
-            this.maList.splice(index, 1);
-        }
-    },
-    computed: {
-        listFiltre() {
-            return this.maList.filter(person => {
-                return person.name.toLowerCase().indexOf(this.query.toLowerCase()) === 0;
-            });
-        }
-    }
-    // router,
-    // template: '<App/>',
-    // components: { App }
+    router,
+    template: '<App/>',
+    components: { App }
 });
